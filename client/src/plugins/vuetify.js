0689a6eed028e3c05051e00b1c485f810838da29
svodnoeraspisanie <svodnoeraspisanie@gmail.com>
2019-08-14 12:54:00 +0300

    j

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,8 +1,11 @@
+/* eslint-disable linebreak-style */
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css';
-import 'roboto-fontface';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import ru from 'vuetify/es5/locale/ru';
+
 
 
 Vue.use(Vuetify);
@@ -10,5 +13,10 @@ Vue.use(Vuetify);
 export default new Vuetify({
   icons: {
     iconfont: 'mdi',
+  },
+
+  lang: {
+    locales: { ru},
+    current: 'ru',
   },
 });
