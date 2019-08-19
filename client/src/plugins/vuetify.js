922b684e05f5a07ed0e0b0b442caf4ed682e32db
svodnoeraspisanie <svodnoeraspisanie@gmail.com>
2019-08-19 15:39:07 +0300

    123

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,10 +1,12 @@
 /* eslint-disable linebreak-style */
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+
 import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import ru from 'vuetify/es5/locale/ru';
+import "ical.js/build/ical.min.js";
 
 Vue.use(Vuetify);
 
