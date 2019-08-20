8baa89bd66c74578c20c6080b228b0ece4488a40
svodnoeraspisanie <svodnoeraspisanie@gmail.com>
2019-08-20 15:23:19 +0300

    123

--- client/src/components/appbar.vue
+++ client/src/components/appbar.vue
@@ -1,11 +1,13 @@
 <template>
 <v-app-bar
-                    clipped-left
+                    :clipped-left="$vuetify.breakpoint.lgAndUp"
                     app
                     dark
                     src="https://picsum.photos/id/466/1920/1080"
                     color="#fcb69f"
-
+                    collapse
+                    min-width="300"
+                    
                 >
                     <template v-slot:img="{ props }">
                         <v-img
@@ -13,43 +15,50 @@
                             gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
                         ></v-img>
                     </template>
-
-                    <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-                        <v-app-bar-nav-icon
+<v-app-bar-nav-icon
                             @click.stop="drawer = !drawer"
                         ></v-app-bar-nav-icon>
+                    <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+                        
                         <span class="hidden-sm-and-down"><router-link to="/"  class="toolbar-title">Свора </router-link></span>
                     </v-toolbar-title>
 
                     <v-toolbar-items>
-                        <v-btn text
+                        <v-btn text 
+                        to="/"
+                        >
+                            Свора</v-btn>
+
+                        
+                        
+                    </v-toolbar-items>
+                    <v-btn icon 
                         to="/predpriyatiya"
                         >
-                            <v-icon left
+                            <v-icon 
                                 >mdi-account-badge-horizontal-outline</v-icon
-                            >Предприятия</v-btn
+                            ></v-btn
                         >
-                        <v-btn text
+                        <v-btn icon
                         to="/raspisanie">
-                            <v-icon left>mdi-calendar</v-icon>Расписание</v-btn
+                            <v-icon >mdi-calendar</v-icon></v-btn
                         >
-                        <v-btn text>
-                            <v-icon left>mdi-android-messages</v-icon
-                            >Сообщения</v-btn
+                        <v-btn icon
+                        to="/">
+                            <v-icon >mdi-help</v-icon></v-btn
                         >
-                    </v-toolbar-items>
                 </v-app-bar>
-
+    
 </template>
 
 <script>
 export default {
-  props: {
+      props: {
     drawer: Boolean,
-    source: String,
+    source: String
   },
-
-};
+    
+}
 </script>
 
 <style scoped>
