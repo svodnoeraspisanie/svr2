551cfde228eb76c1579c358062418f86ba367d95
yasn0st <yasnost.pub@gmail.com>
2020-01-11 00:34:55 +0300

    1

--- client/src/db.js
+++ client/src/db.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-export const db = firebase.initializeApp({ projectId: 'svora-6f3df' }).firestore();
+const config = {
+    apiKey: "AIzaSyBUrnt50CnPcbG0EvIVsoQawYC_rHyzlQ0",
+    authDomain: "svora-6f3df.firebaseapp.com",
+    databaseURL: "https://svora-6f3df.firebaseio.com/",
+    storageBucket: "svora-6f3df.appspot.com",
+    projectId: 'svora-6f3df',
+  };
+export const db = firebase.initializeApp(config).firestore();
 
 const { Timestamp, GeoPoint } = firebase.firestore
 export { Timestamp, GeoPoint }
